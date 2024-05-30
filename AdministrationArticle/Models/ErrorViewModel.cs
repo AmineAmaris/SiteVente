29b22b9e3ba3716c17f9f78033bdaa25261fdb33
Amine <mohamedamine.debbabi@gmail.com>
2024-05-30 13:58:18 +0200

    creation du projet

---
+++ AdministrationArticle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdministrationArticle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
